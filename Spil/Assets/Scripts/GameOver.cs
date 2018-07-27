1db139c594aadf1f0b8f22a08d342978378a14ac
DESKTOP-1RSAP7A\UNFGRUPPE6PC01 <teknisk@game.unf.dk>
2018-07-27 21:12:36 +0200

    Deathzones virker nu

---
+++ Spil/Assets/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+
+    public bool playerIsDead;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerIsDead)
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+	}
+}
