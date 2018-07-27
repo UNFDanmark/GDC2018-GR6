a830fa85f4ea923a5fa3c6d3f9ea2e195be34193
DESKTOP-1RSAP7A\UNFGRUPPE6PC01 <teknisk@game.unf.dk>
2018-07-27 17:49:28 +0200

    spiller bevægelse tilføjet

---
+++ Spil/Assets/Scripts/Players/P1Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Move : MonoBehaviour {
+
+
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private Rigidbody p1Rb;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Move(moveSpeed);
+    }
+
+    void Move(float speed)
+    {
+
+        Vector3 velocityMove = new Vector3(speed * Input.GetAxis("Horizontal"), p1Rb.velocity.y, p1Rb.velocity.z);
+        p1Rb.velocity = velocityMove;
+    }
+}
---
+++ Spil/Assets/Scripts/Players/P2Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Move : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private Rigidbody p2Rb;
+    [SerializeField]
+    private float jumpForce;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move(moveSpeed);
+        
+        if (Input.GetAxis("P2 Jump") > 0)
+        {
+            Jump(jumpForce);
+        }
+    }
+
+    void Move(float speed)
+    {
+
+        Vector3 velocityMove = new Vector3(speed * Input.GetAxis("P2 Horizontal"), p2Rb.velocity.y, p2Rb.velocity.z);
+        p2Rb.velocity = velocityMove;
+    }
+
+    void Jump(float force)  
+    {
+        Vector3 velocityMove = new Vector3(p2Rb.velocity.x, force, p2Rb.velocity.z);
+        p2Rb.velocity = velocityMove;
+    }
+}
