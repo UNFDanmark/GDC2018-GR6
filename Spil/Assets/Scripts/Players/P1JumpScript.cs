fde2c427189de67860b8f1fa30efc6cad9ef1167
DESKTOP-1RSAP7A\UNFGRUPPE6PC01 <teknisk@game.unf.dk>
2018-07-29 17:24:40 +0200

    Hop virker nu som det skal

---
+++ Spil/Assets/Scripts/Players/P1JumpScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1JumpScript : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody myRigidbody;
+    [SerializeField]
+    private float jumpForce;
+
+    private bool grounded;
+    // Use this for initialization
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Jump") > 0 && grounded)
+        {
+            Jump(jumpForce);
+        }
+    }
+
+    private void OnTriggerEnter(Collider ground)
+    {
+        if (ground.tag == "Ground")
+        {
+            grounded = true;
+        }
+
+    }
+    private void OnTriggerExit(Collider ground)
+    {
+        if (ground.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+    void Jump(float force)
+    {
+        Vector3 velocityMove = new Vector3(myRigidbody.velocity.x, force, myRigidbody.velocity.z);
+        myRigidbody.velocity = velocityMove;
+    }
+}
--- Spil/Assets/Scripts/Players/P1Move.cs
+++ Spil/Assets/Scripts/Players/P1Move.cs
@@ -11,10 +11,7 @@ public class P1Move : MonoBehaviour {
     //private float airSpeed;
     [SerializeField]
     private Rigidbody p1Rb;
-    [SerializeField]
-    private float jumpForce;
-
-    private bool grounded;
+    
 	// Update is called once per frame
 
 	void Update () {
@@ -25,27 +22,8 @@ public class P1Move : MonoBehaviour {
     {
         Move(moveSpeed);
         
-        if (Input.GetAxis("Jump") > 0 && grounded)
-        {
-            Jump(jumpForce);
-        }
     }
 
-    //de næste to funktioner bruges til at registrere om man er på jorden
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.CompareTag("Ground"))
-        {
-            grounded = true;
-        }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.CompareTag("Ground"))
-        {
-            grounded = false;
-        }
-    }
 
     void Move(float speed)
     {
@@ -53,9 +31,5 @@ public class P1Move : MonoBehaviour {
         Vector3 velocityMove = new Vector3(speed * Input.GetAxis("Horizontal"), p1Rb.velocity.y, p1Rb.velocity.z);
         p1Rb.velocity = velocityMove;
     }
-    void Jump(float force)
-    {
-        Vector3 velocityMove = new Vector3(p1Rb.velocity.x, force, p1Rb.velocity.z);
-        p1Rb.velocity = velocityMove;
-    }
+
 }
