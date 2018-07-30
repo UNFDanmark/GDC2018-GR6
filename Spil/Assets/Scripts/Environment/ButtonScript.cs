f17e20ec16026ac5a301e610500cf47ead5df8d2
DESKTOP-1RSAP7A\UNFGRUPPE6PC01 <teknisk@game.unf.dk>
2018-07-30 09:49:50 +0200

    Button script made, Buttons placed, SwitchZones placed.

---
+++ Spil/Assets/Scripts/Environment/ButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+    public GameObject currentZone;
+    public GameObject newZone;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Light") || collision.gameObject.CompareTag("Dark"))
+        {
+            currentZone.SetActive(false);
+            newZone.SetActive(true);
+        }
+    }
+}
