bd25a44cafa2bf2373b86d55591b8a9f61ac9fb5
DESKTOP-1RSAP7A\UNFGRUPPE6PC01 <teknisk@game.unf.dk>
2018-07-31 19:22:14 +0200

    Walking sound fixed

--- Spil/Assets/Scripts/Players/P1Move.cs
+++ Spil/Assets/Scripts/Players/P1Move.cs
@@ -8,22 +8,36 @@ public class P1Move : MonoBehaviour {
     [SerializeField]
     private float moveSpeed;
     private Rigidbody p1Rb;
+    public AudioSource walk;
 
     private void Awake()
     {
         p1Rb = GetComponent<Rigidbody>();
+        walk = GetComponent<AudioSource>();
     }
 
     private void FixedUpdate()
     {
-        Move(moveSpeed);      
+        Move(moveSpeed);
     }
+
 
     void Move(float speed)
     {
 
         Vector3 velocityMove = new Vector3(speed * Input.GetAxis("Horizontal"), p1Rb.velocity.y, p1Rb.velocity.z);
         p1Rb.velocity = velocityMove;
+        if (p1Rb.velocity.x != 0)
+        {
+            if (!walk.isPlaying)
+            {
+                walk.Play();
+            }
+        }
+        else
+        {
+            walk.Stop();
+        }
     }
-    
+
 }
