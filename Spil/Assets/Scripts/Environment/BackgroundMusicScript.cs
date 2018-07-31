d7e90dade826d2235a052f52e050a863f60105a1
DESKTOP-1RSAP7A\UNFGRUPPE6PC01 <teknisk@game.unf.dk>
2018-08-01 00:08:42 +0200

    final touches

--- Spil/Assets/Scripts/Environment/BackgroundMusicScript.cs
+++ Spil/Assets/Scripts/Environment/BackgroundMusicScript.cs
@@ -13,8 +13,6 @@ public class BackgroundMusicScript : MonoBehaviour {
             GameObject newSource = Instantiate(source);
             DontDestroyOnLoad(newSource);
         }
-       
-
-        
+               
 	}
 }
