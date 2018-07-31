e5b7d98b8f3f6f26fc288d7ac001586c1e2eb380
DESKTOP-1RSAP7A\UNFGRUPPE6PC01 <teknisk@game.unf.dk>
2018-07-31 11:18:30 +0200

    Player models implemented!!!!!! YES

---
+++ Spil/Assets/Scripts/Players/RotateScriptP1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScriptP1 : MonoBehaviour {
+
+
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.eulerAngles = new Vector3(-90, -120, 0);
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.eulerAngles = new Vector3(-90, 120, 0);
+        }
+    }
+}
